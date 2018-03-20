3dc98a6f6f1aecfe84e30e991120b6ed216fbe10
SongLeLe <skj198568@163.com>
2018-03-20 16:18:58 +0800

    去除无用文件处理函数

--- ClassLibrary/ClHttp.php
+++ ClassLibrary/ClHttp.php
@@ -41,7 +41,7 @@ class ClHttp
     public static function getServerDomain($with_protocol = true)
     {
         $protocol = '';
-        if($with_protocol){
+        if ($with_protocol) {
             $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
         }
         return $protocol . $_SERVER['HTTP_HOST'];
@@ -147,7 +147,7 @@ class ClHttp
     public static function http($url, $params = [], $duration = 0, $is_debug = false, $result_type = 'json')
     {
         $config = ['post' => $params];
-        if($result_type == 'json'){
+        if ($result_type == 'json') {
             $config['content_type'] = 'application/json';
         }
         $key = ClCache::getKey($duration, $url, $params);
@@ -552,180 +552,6 @@ class ClHttp
         }
         $exp[1] = trim($exp[1]);
         return $exp;
-    }
-
-    /**
-     * 采集远程文件
-     * @access public
-     * @param string $remote 远程文件名
-     * @param string $local_absolute_file 本地保存文件名
-     */
-    public static function downloadCurl($remote, $local_absolute_file)
-    {
-        //创建文件夹
-        ClFile::dirCreate($local_absolute_file);
-        //下载
-        $cp = curl_init(urlencode($remote));
-        $fp = fopen($local_absolute_file, "w");
-        curl_setopt($cp, CURLOPT_FILE, $fp);
-        curl_setopt($cp, CURLOPT_HEADER, 0);
-        curl_exec($cp);
-        curl_close($cp);
-        fclose($fp);
-    }
-
-    /**
-     * 使用 fsockopen 通过 HTTP 协议直接访问(采集)远程文件
-     * 如果主机或服务器没有开启 CURL 扩展可考虑使用
-     * fsockopen 比 CURL 稍慢,但性能稳定
-     * @static
-     * @access public
-     * @param string $url 远程URL
-     * @param array $conf 其他配置信息
-     *        int   limit 分段读取字符个数
-     *        string post  post的内容,字符串或数组,key=value&形式
-     *        string cookie 携带cookie访问,该参数是cookie内容
-     *        string ip    如果该参数传入,$url将不被使用,ip访问优先
-     *        int    timeout 采集超时时间
-     *        bool   block 是否阻塞访问,默认为true
-     * @return mixed
-     */
-    public static function fsockopenDownload($url, $conf = array())
-    {
-        $return = '';
-        if (!is_array($conf)) return $return;
-
-        $matches = parse_url($url);
-        !isset($matches['host']) && $matches['host'] = '';
-        !isset($matches['path']) && $matches['path'] = '';
-        !isset($matches['query']) && $matches['query'] = '';
-        !isset($matches['port']) && $matches['port'] = '';
-        $host = $matches['host'];
-        $path = $matches['path'] ? $matches['path'] . ($matches['query'] ? '?' . $matches['query'] : '') : '/';
-        $port = !empty($matches['port']) ? $matches['port'] : 80;
-
-        $conf_arr = array(
-            'limit' => 0,
-            'post' => '',
-            'cookie' => '',
-            'ip' => '',
-            'timeout' => 15,
-            'block' => TRUE,
-            'content_type' => 'application/x-www-form-urlencoded'
-        );
-        $conf_arr = array_merge($conf_arr, $conf);
-        $user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36 Core/1.47.640.400 QQBrowser/9.4.8309.400';
-        if(isset($_SERVER['HTTP_USER_AGENT'])){
-            $user_agent = $_SERVER['HTTP_USER_AGENT'];
-        }
-        $limit = $conf_arr['limit'];
-        $post = $conf_arr['post'];
-        $cookie = $conf_arr['cookie'];
-        $ip = $conf_arr['ip'];
-        $timeout = $conf_arr['timeout'];
-        $block = $conf_arr['block'];
-        $content_type = $conf_arr['content_type'];
-        if ($post) {
-            if (is_array($post)) {
-                $post = http_build_query($post);
-            }
-            $out = "POST $path HTTP/1.0\r\n";
-            $out .= "Accept: */*\r\n";
-            //$out .= "Referer: $boardurl\r\n";
-            $out .= "Accept-Language: zh-cn\r\n";
-            $out .= "Content-Type: $content_type\r\n";
-            $out .= "User-Agent: $user_agent\r\n";
-            $out .= "Host: $host\r\n";
-            $out .= 'Content-Length: ' . strlen($post) . "\r\n";
-            $out .= "Connection: Close\r\n";
-            $out .= "Cache-Control: no-cache\r\n";
-            $out .= "Cookie: $cookie\r\n\r\n";
-            $out .= $post;
-        } else {
-            $out = "GET $path HTTP/1.0\r\n";
-            $out .= "Accept: */*\r\n";
-            //$out .= "Referer: $boardurl\r\n";
-            $out .= "Accept-Language: zh-cn\r\n";
-            $out .= "User-Agent: $user_agent\r\n";
-            $out .= "Host: $host\r\n";
-            $out .= "Connection: Close\r\n";
-            $out .= "Cookie: $cookie\r\n\r\n";
-        }
-        $fp = @fsockopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout);
-        if (!$fp) {
-            return '';
-        } else {
-            stream_set_blocking($fp, $block);
-            stream_set_timeout($fp, $timeout);
-            @fwrite($fp, $out);
-            $status = stream_get_meta_data($fp);
-            if (!$status['timed_out']) {
-                while (!feof($fp)) {
-                    if (($header = @fgets($fp)) && ($header == "\r\n" || $header == "\n")) {
-                        break;
-                    }
-                }
-
-                $stop = false;
-                while (!feof($fp) && !$stop) {
-                    $data = fread($fp, ($limit == 0 || $limit > 8192 ? 8192 : $limit));
-                    $return .= $data;
-                    if ($limit) {
-                        $limit -= strlen($data);
-                        $stop = $limit <= 0;
-                    }
-                }
-            }
-            @fclose($fp);
-            return $return;
-        }
-    }
-
-    /**
-     * 下载文件
-     * 可以指定下载显示的文件名，并自动发送相应的Header信息
-     * 如果指定了content参数，则下载该参数的内容
-     * @param string $file_absolute 下载文件绝对地址
-     * @param string $show_name 下载显示的文件名
-     * @param string $content 下载的内容
-     * @param int $expire 下载内容浏览器缓存时间
-     * @throws Exception
-     */
-    public static function downloadForClient($file_absolute, $show_name = '', $content = '', $expire = 180)
-    {
-        if (is_file($file_absolute)) {
-            $length = filesize($file_absolute);
-        } elseif ($content != '') {
-            $length = strlen($content);
-        } else {
-            throw new Exception($file_absolute . '下载文件不存在！');
-        }
-        if (empty($show_name)) {
-            $show_name = basename($file_absolute);
-        }
-        if (!empty($filename)) {
-            $finfo = new \finfo(FILEINFO_MIME);
-            $type = $finfo->file($filename);
-        } else {
-            $type = "application/octet-stream";
-        }
-        //发送Http Header信息 开始下载
-        header("Pragma: public");
-        header("Cache-control: max-age=" . $expire);
-        //header('Cache-Control: no-store, no-cache, must-revalidate');
-        header("Expires: " . gmdate("D, d M Y H:i:s", time() + $expire) . "GMT");
-        header("Last-Modified: " . gmdate("D, d M Y H:i:s", time()) . "GMT");
-        header("Content-Disposition: attachment; filename=" . $show_name);
-        header("Content-Length: " . $length);
-        header("Content-type: " . $type);
-        header('Content-Encoding: none');
-        header("Content-Transfer-Encoding: binary");
-        if ($content == '') {
-            readfile($file_absolute);
-        } else {
-            echo($content);
-        }
-        exit();
     }
 
 }

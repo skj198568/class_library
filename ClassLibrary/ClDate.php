96c7dfb645920afb6f0285f1aa9520946ecf9266
SongLeLe <skj198568@163.com>
2018-05-20 16:55:39 +0800

    添加毫秒函数

--- ClassLibrary/ClDate.php
+++ ClassLibrary/ClDate.php
@@ -59,6 +59,15 @@ class ClDate {
     }
 
     /**
+     * 获取毫秒
+     * @return float
+     */
+    public static function getMillisecond() {
+        list($s1, $s2) = explode(' ', microtime());
+        return (float)sprintf('%.0f', (floatval($s1) + floatval($s2)) * 1000);
+    }
+
+    /**
      * 获取微秒
      * @return mixed
      */

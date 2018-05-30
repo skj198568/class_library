d4777cc38fd11670c8adeabbd5c1f81212739e02
SongLeLe <skj198568@163.com>
2018-05-30 10:19:26 +0800

    缓存解密后的数据

--- ClassLibrary/ClCrypt.php
+++ ClassLibrary/ClCrypt.php
@@ -43,40 +43,56 @@ class ClCrypt {
 
     /**
      * 解密字符串
-     * @param string $string 字符串
-     * @param string $key 加密key
-     * @return string
+     * @param $string 字符串
+     * @param $key 加密key
+     * @param bool $get_left_time 获取剩余时间
+     * @return bool|int|string
      */
-    public static function decrypt($string, $key) {
-        $key    = md5($key);
-        $string = str_replace(array('-', '_'), array('+', '/'), $string);
-        $mod4   = strlen($string) % 4;
-        if ($mod4) {
-            $string .= substr('====', $mod4);
-        }
-        $string = base64_decode($string);
-        $x      = 0;
-        $len    = strlen($string);
-        $l      = strlen($key);
-        $char   = $str = '';
-        for ($i = 0; $i < $len; $i++) {
-            if ($x == $l) $x = 0;
-            $char .= substr($key, $x, 1);
-            $x++;
-        }
-        for ($i = 0; $i < $len; $i++) {
-            if (ord(substr($string, $i, 1)) < ord(substr($char, $i, 1))) {
-                $str .= chr((ord(substr($string, $i, 1)) + 256) - ord(substr($char, $i, 1)));
-            } else {
-                $str .= chr(ord(substr($string, $i, 1)) - ord(substr($char, $i, 1)));
+    public static function decrypt($string, $key, $get_left_time = false) {
+        $cache_key      = ClCache::getKey([$string, $key]);
+        $decrypt_string = cache($cache_key);
+        if ($decrypt_string === false) {
+            $key    = md5($key);
+            $string = str_replace(array('-', '_'), array('+', '/'), $string);
+            $mod4   = strlen($string) % 4;
+            if ($mod4) {
+                $string .= substr('====', $mod4);
             }
+            $string = base64_decode($string);
+            $x      = 0;
+            $len    = strlen($string);
+            $l      = strlen($key);
+            $char   = $str = '';
+            for ($i = 0; $i < $len; $i++) {
+                if ($x == $l) $x = 0;
+                $char .= substr($key, $x, 1);
+                $x++;
+            }
+            for ($i = 0; $i < $len; $i++) {
+                if (ord(substr($string, $i, 1)) < ord(substr($char, $i, 1))) {
+                    $str .= chr((ord(substr($string, $i, 1)) + 256) - ord(substr($char, $i, 1)));
+                } else {
+                    $str .= chr(ord(substr($string, $i, 1)) - ord(substr($char, $i, 1)));
+                }
+            }
+            $decrypt_string = base64_decode($str);
+            //缓存
+            cache($cache_key, $decrypt_string, 3600);
         }
-        $string = base64_decode($str);
-        $expire = substr($string, 0, 10);
-        if ($expire > 0 && $expire < time()) {
-            return '';
+        if ($get_left_time) {
+            $expire = substr($decrypt_string, 0, 10);
+            if ($expire > 0 && $expire < time()) {
+                return 0;
+            } else {
+                return $expire - time();
+            }
+        } else {
+            $expire = substr($decrypt_string, 0, 10);
+            if ($expire > 0 && $expire < time()) {
+                return '';
+            }
+            return substr($decrypt_string, 10);
         }
-        $string = substr($string, 10);
-        return $string;
     }
+
 }

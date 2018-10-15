419577e57311c8bab3dac0b7832aca715351ede6
SongLeLe <skj198568@163.com>
2018-10-15 10:12:22 +0800

    身份证验证修改

--- ClassLibrary/ClVerify.php
+++ ClassLibrary/ClVerify.php
@@ -80,11 +80,61 @@ class ClVerify {
 
     /**
      * 是否是身份证号
-     * @param $str
+     * @param $id_card
      * @return bool
      */
-    public static function isIdCard($str) {
-        return preg_match('/\d{15}|\d{18}/', $str) === 1;
+    public static function isIdCard($id_card) {
+        $id_card = self::isIdCardTo18Card($id_card);
+        if (strlen($id_card) != 18) {
+            return false;
+        }
+        $id_cardBase = substr($id_card, 0, 17);
+        return (self::isIdCardGetVerifyNum($id_cardBase) == strtoupper(substr($id_card, 17, 1)));
+    }
+
+    /**
+     * 格式化15位身份证号码为18位
+     * @param $id_card
+     * @return bool|string
+     */
+    private static function isIdCardTo18Card($id_card) {
+        $id_card = trim($id_card);
+        if (strlen($id_card) == 18) {
+            return $id_card;
+        }
+        if (strlen($id_card) != 15) {
+            return false;
+        }
+        // 如果身份证顺序码是996 997 998 999,这些是为百岁以上老人的特殊编码
+        if (array_search(substr($id_card, 12, 3), array('996', '997', '998', '999')) !== false) {
+            $id_card = substr($id_card, 0, 6) . '18' . substr($id_card, 6, 9);
+        } else {
+            $id_card = substr($id_card, 0, 6) . '19' . substr($id_card, 6, 9);
+        }
+        $id_card = $id_card . self::isIdCardGetVerifyNum($id_card);
+        return $id_card;
+    }
+
+    /**
+     * 计算身份证校验码,根据国家标准gb 11643-1999
+     * @param $id_cardBase
+     * @return bool|mixed
+     */
+    private static function isIdCardGetVerifyNum($id_cardBase) {
+        if (strlen($id_cardBase) != 17) {
+            return false;
+        }
+        // 加权因子
+        $factor = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+        // 校验码对应值
+        $verify_number_list = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+        $checksum           = 0;
+        for ($i = 0; $i < strlen($id_cardBase); $i++) {
+            $checksum += substr($id_cardBase, $i, 1) * $factor[$i];
+        }
+        $mod           = $checksum % 11;
+        $verify_number = $verify_number_list[$mod];
+        return $verify_number;
     }
 
     /**
